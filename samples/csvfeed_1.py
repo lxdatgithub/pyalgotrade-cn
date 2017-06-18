b055ac1690942a653ee0a3dbc369fc8046d0b56a
xdliu <lxd5apple@icloud.com>
2017-06-18 11:38:07 +0800

    变更readme编码为utf8

--- samples/csvfeed_1.py
+++ samples/csvfeed_1.py
@@ -1,6 +1,7 @@
 from pyalgotrade.feed import csvfeed
 
 feed = csvfeed.Feed("Date", "%Y-%m-%d")
-feed.addValuesFromCSV("quandl_gold_2.csv")
+# feed.addValuesFromCSV("./data/quandl_gold_2.csv")
+feed.addValuesFromCSV("/home/xdliu/workspace/open_source_tools/pyalgotrade-cn/samples/data/VEU-2007-yahoofinance.csv")
 for dateTime, value in feed:
     print dateTime, value
